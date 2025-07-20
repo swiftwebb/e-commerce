10e78abb77b6c1c7a3854e7eeb0ae37b23909661
swiftwebb <swiftwebbb@gmail.com>
2025-07-20 16:18:03 +0100

    message

--- ecom/settings.py
+++ ecom/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'unsafe-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = os.getenv('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ['e-commerce-quse.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
