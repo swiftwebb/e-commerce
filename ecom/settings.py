03955a543b0d704941d18b0f94804acccaff7590
swiftwebb <swiftwebbb@gmail.com>
2025-08-22 23:31:51 +0100

    kk

--- ecom/settings.py
+++ ecom/settings.py
@@ -17,7 +17,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
     # Use Render's Postgres database in production
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600,ssl_require=True)
     }
 else:
     # Local development DB: using SQLite by default
