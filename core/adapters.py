9d8c0ee48a3862cd582287f5c5fb70e0bd9e5b13
swiftwebb <swiftwebbb@gmail.com>
2025-08-22 19:40:38 +0100

    kk

---
+++ core/adapters.py
@@ -0,0 +1,17 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.exceptions import ImmediateHttpResponse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class MySocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        # If a user with this email already exists, link accounts
+        email = sociallogin.account.extra_data.get('email')
+        if email:
+            try:
+                user = User.objects.get(email=email)
+                sociallogin.connect(request, user)
+            except User.DoesNotExist:
+                pass
